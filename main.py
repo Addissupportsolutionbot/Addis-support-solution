70ca9db4c9eaaf3f7dba40f3949af270ccba8863
Addissupportsolutionbot <mikyaswudie12@gmail.com>
2026-05-22 22:49:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8794659915:AAGLfFmA74Ig1rc4d0A6WzAaDBFEiG8XsDA")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome to Addis Support Solution 🇪🇹")
+    await update.message.reply_text(
+        "Welcome to Addis Support Solution 🇪🇹"
+    )
 
-async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working online 👍")
+def main():
+    print("Bot is ONLINE now!")
 
-app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+    app.add_handler(CommandHandler("start", start))
 
-print("Bot running...")
-app.run_polling()
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
